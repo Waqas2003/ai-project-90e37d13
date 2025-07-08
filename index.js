feb8b662d666e3a49095d9112a90cb21d9e9e24c
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-08 20:08:49 +0500

    Add index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to my React Webpage!</h1>
+      <p>This is a simple React webpage.</p>
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
